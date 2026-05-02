f556406ddc3bbbd587062495744c7d47dac8987d
Prathamesh-Marathe <maratheprathmesh121@gmail.com>
2026-05-03 01:38:30 +0530

    Added Delete Update Mark functionality

--- task-cli.js
+++ task-cli.js
@@ -2,16 +2,19 @@
 
 const fs = require('fs');
 
+// ------------------------------------------- Display Task List -------------------------------------------------------
 function displayTasks(tasks) {
     console.log("\n******************** Tasks ********************");
 
     for(task of tasks) {
         console.log("ID : ", task.id);
         console.log("Task : ", task.description);
+        console.log("Status : ", task.status);
         console.log("-----------------------------------------------");
     }
 }
 
+// ------------------------------------------- Add New Task ------------------------------------------------------------
 function addTask(task) {
     if(task === undefined)
         return 0;
@@ -38,6 +41,7 @@ function addTask(task) {
     return id;
 }
 
+// ------------------------------------------- List Tasks ------------------------------------------------------------------
 function listTasks(status) {
     const taskStatus = ['todo', 'in-progress', 'done'];
 
@@ -72,11 +76,85 @@ function listTasks(status) {
     return true;
 }
 
+// ------------------------------------------------------- Update Task -----------------------------------------------------
+function updateTask(id, updatedTask) {
+    let tasks = [];
+    let data = fs.readFileSync('task.json', 'utf-8');
+
+    tasks = data ? JSON.parse(data) : [];
+
+    if(tasks.length == 0) {
+        console.log("Task List is Empty!");
+        return false;
+    }
+
+    for(let task of tasks) {
+        if(task.id === id) {
+            task.description = updatedTask;
+            fs.writeFileSync('task.json', JSON.stringify(tasks, null, 2));
+            return true;
+        }
+    }
+
+    console.log(`Task with ID ${id} is not present in list`);
+    return false;
+}
+
+// --------------------------------------------------- Delete Task --------------------------------------------------------------
+function deleteTask(id) {
+    let tasks = [];
+    let data = fs.readFileSync('task.json', 'utf-8');
+    tasks = data ? JSON.parse(data) : [];
+
+    if(tasks.length == 0){
+        console.log("Task List is Empty");
+        return false;
+    }
+
+    for(let i=tasks.length-1; i>=0; i--) {
+        if(tasks[i].id == id) {
+            tasks.splice(i, 1);
+            fs.writeFileSync('task.json', JSON.stringify(tasks, null, 2));
+            return true;
+        }
+    }
+
+    console.log(`Task with ID ${id} NOT FOUND`);
+    return false;
+}
+
+// ---------------------------------------------- Marking a task as in progress or done ----------------------------------------
+function mark(id, status) {
+    let tasks = [];
+    let data = fs.readFileSync('task.json', 'utf-8');
+
+    tasks = data ? JSON.parse(data) : [];
+
+    if(tasks.length == 0) {
+        console.log("Task List is Empty!");
+        return false;
+    }
+
+    for(let task of tasks) {
+        if(task.id === id) {
+            if(status === "done") {
+                deleteTask(id);
+            } else {
+                task.status = status;
+                fs.writeFileSync('task.json', JSON.stringify(tasks, null, 2));
+            }
+            return true;
+        }
+    }
+
+    console.log(`Task with ID ${id} not found`);
+}
+
 const args = process.argv.slice(2);
 const command = args[0];
 
 switch(command) {
-    case "add":
+    case "add": {
         const task = args[1];
         const id = addTask(task);
 
@@ -87,11 +165,69 @@ switch(command) {
         }
 
         break;
+    }
 
-    case "list":
+    case "list": {
         const status = args[1];
         let isTaskPresent = listTasks(status);
         if(!isTaskPresent)
             console.log(`there is no task with status ${status}`);
         break;
+    }
+
+    case "update": {
+        const id = parseInt(args[1]);
+        const updatedTask = args[2];
+        if(Number.isNaN(id)) {
+            console.log("Enter Valid Task ID");
+        } else if(updatedTask === undefined || updatedTask === "") {
+            console.log("Please Enter Task to Update Old Task");
+        } else {
+            const res = updateTask(id, updatedTask);
+            if(res) {
+                console.log("Updated Sucessfully!");
+            } 
+        }
+        break;
+    }
+
+    case "delete": {
+        const id = args[1];
+        if(Number.isNaN(id)) {
+            console.log("Enter Valid Task ID");
+        } else {
+            const res = deleteTask(parseInt(id));
+            if(res)
+                console.log(`Task with ID ${id} deleted Successfully!`);
+        }
+    }
+
+    case "mark-in-progress": {
+        const id = parseInt(args[1]);
+        if(Number.isNaN(id)) {
+            console.log("Enter Valid Task ID");
+        } else {
+            const res = mark(id, "in-progress");
+            if(res)
+                console.log(`Status Updated Successfully for Task ID ${id}`);
+        }
+        break;
+    }
+
+    case "mark-done" : {
+        const id = parseInt(args[1]);
+        if(Number.isNaN(id)) {
+            console.log("Enter Valid Task ID");
+        } else {
+            const res = mark(id, "done");
+            if(res)
+                console.log(`Status Updated Successfully for Task ID ${id}`);
+        }
+        break;
+    }
+
+    default : {
+        console.log("Enter VALID Command!");
+        break;
+    }
 }
